ce8de350404ad4f86d139a8b3e40fcab095bf454
MilX_ql89 <MilX_ql89@top-academy.ru>
2022-09-02 19:08:18 +0300

    не работает 4й пункт не придумал как вводить цифры с буквами

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -6,21 +6,16 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	cout << "     Введите двоичное число:  ";
+	cout << "Введите шестнадцатиричное число:  ";
 
-	int vvod, temp, razr = 0, j = 0, array[10], sum = 0, result = 1, exp;
+	int vvod, temp, razr = 0, j = 0, array[10], sum = 0, result = 1, exp, _16;
+	char vvod1;
 	cin >> vvod;
 	temp = vvod;
-	cout << " Двоичное число " << temp << " в десятичной системе будет :  ";
-	//кол-во разрядов во введенном числе
-	while (vvod != 0)
-	{
-		vvod /= 10;
-		++razr;
-	}
-	//cout << razr << "|";
+	cout << " Шестнадцатиричное число " << temp << " в десятичной системе будет :  ";
 
-	//cout << endl;
+	
+
 	// загоняем введенное число в массив
 	do
 	{
@@ -29,14 +24,28 @@ int main()
 
 	} while (temp != 0);
 
+
+	//кол-во разрядов во введенном числе
+	while (vvod != 0)
+	{
+		vvod /= 10;
+		++razr;
+	}
+	//cout << razr << "|" << endl;
+
+	//cout << endl;
 	exp = razr - 1;
 
 	for (int i = j - 1; i >= 0; i--)
+
 	{
-		result = pow(2, exp);
-		--exp;
+		//cout << array[i] << " - " << endl;
+
+		result = pow(16, exp);
 		array[i] *= result;
+		--exp;
 		//cout << array[i] << " + ";
+
 	}
 
 	//cout << endl;
@@ -44,8 +53,9 @@ int main()
 	for (int i = 0; i < razr; i++)
 	{
 		sum += array[i];
-		
+		//cout << sum << endl;
 	}
 
 	cout << sum << endl;
+
 }
